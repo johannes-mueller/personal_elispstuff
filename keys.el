24a65e7437fa4386ededcf0b780985273ff87789
Johannes Mueller <github@johannes-mueller.org>
2015-04-01 18:01:42 +0200

    Defined multiple cursors keys

--- keys.el
+++ keys.el
@@ -19,6 +19,11 @@
 (global-set-key (kbd "C-c C-j") 'johmue/comment-current-line)
 (global-set-key (kbd "C-c j") 'johmue/uncomment-current-line)
 
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
 (define-key global-map [f9]          'copy-for-paste)
 (define-key global-map [f10]         'paste-copied)
 
