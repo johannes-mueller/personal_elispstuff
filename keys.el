02ade021d37f46eed35b0857cbe8346f7e8b655b
Johannes Mueller <github@johannes-mueller.org>
2020-04-11 01:58:29 +0200

    Add windmove key bindings

--- keys.el
+++ keys.el
@@ -32,7 +32,12 @@
 (define-key global-map [f9]          'copy-for-paste)
 (define-key global-map [f10]         'paste-copied)
 
-					;(define-key global-map [f11]         'ispell-buffer)
+(global-set-key (kbd "C-x <left>") 'windmove-left)
+(global-set-key (kbd "C-x <right>") 'windmove-right)
+(global-set-key (kbd "C-x <up>") 'windmove-up)
+(global-set-key (kbd "C-x <down>") 'windmove-down)
+
+;(define-key global-map [f11]         'ispell-buffer)
 ;(define-key global-map [(meta f11)]  'flyspell-auto-correct-word)
 
 ;(define-key global-map '(meta control kp-right) 'bigframe)
