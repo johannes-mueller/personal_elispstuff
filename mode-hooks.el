3b8dc44bb2ac4ac6ce98c3c1329531f492e1ef57
Johannes Mueller <github@johannes-mueller.org>
2020-05-17 12:12:23 +0200

    Add smart parens funcionality, especially for elixir mode

--- mode-hooks.el
+++ mode-hooks.el
@@ -197,5 +197,11 @@
 (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv)
 (add-hook 'projectile-after-switch-project-hook 'auto-virtualenv-set-virtualenv)
 
+(add-to-list 'elixir-mode-hook
+             (defun auto-activate-ruby-end-mode-for-elixir-mode ()
+               (set (make-variable-buffer-local 'ruby-end-expand-keywords-before-re)
+                    "\\(?:^\\|\\s-+\\)\\(?:do\\)")
+               (set (make-variable-buffer-local 'ruby-end-check-statement-modifiers) nil)
+               (ruby-end-mode +1)))
 
 ;;; mode-hooks.el ends here
