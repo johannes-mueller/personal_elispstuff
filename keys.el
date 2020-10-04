750b5294f5f068a13e5b29d1a9cd63fa7476e2da
Johannes Mueller <github@johannes-mueller.org>
2020-10-04 18:34:25 +0200

    Some more stuff on window management

--- keys.el
+++ keys.el
@@ -43,6 +43,8 @@
 (global-set-key (kbd "C-x <up>") 'windmove-up)
 (global-set-key (kbd "C-x <down>") 'windmove-down)
 
+(global-set-key (kbd "C-x C-2") 'delete-other-windows-vertically)
+
 ;(define-key global-map [f11]         'ispell-buffer)
 ;(define-key global-map [(meta f11)]  'flyspell-auto-correct-word)
 
